ba0cca632157f56cb836504b691f9de1edc95d2f
Dave <david.luettmann@gmx.de>
2020-05-04 14:21:30 +0200

    add client-server example

---
+++ examples/client.py
@@ -0,0 +1,21 @@
+import socket
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = '127.0.0.1'
+port = 10001
+buffer_size = 1024
+
+message = "Hello Server"
+
+try:
+    socket.sendto(message.encode(), (server_address, port))
+    print("Send to server %s" % message)
+    
+    print("Waiting for response...")
+    data, server = socket.recvfrom(buffer_size)
+    print("Received message from server %s" % data.decode())
+
+finally:
+    socket.close()
+    print('Socket closed')
---
+++ examples/server.py
@@ -0,0 +1,24 @@
+import socket
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = '127.0.0.1'
+port = 10001
+buffer_size = 1024
+
+message = "Hello Client"
+
+print("Server up and running at %s:%s" % (server_address, port))
+
+socket.bind((server_address, port))
+
+while True:
+    print("Waiting for message...")
+
+    data, address = socket.recvfrom(buffer_size)
+    print("Received message from client %s" % address[0])
+    print("Message: %s" % data.decode())
+
+    if data:
+        socket.sendto(str.encode(message), address)
+        print("Replied to client: %s" % message)
