d9e6c8d7117c7fb02b21cb7f93772bf2e869cf61
Dave <david.luettmann@gmx.de>
2020-05-04 21:11:37 +0200

    change socket to multicast

--- src/localshit/service_announcement.py
+++ src/localshit/service_announcement.py
@@ -5,7 +5,7 @@ Adapted from https://stackoverflow.com/questions/21089268/python-service-discove
 """
 
 from time import sleep
-from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
+from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, IPPROTO_UDP, IP_MULTICAST_TTL, IPPROTO_IP
 from stop import StoppableThread
 
 import uuid
@@ -18,22 +18,21 @@ logging.basicConfig(
 
 
 class ServiceAnnouncement(StoppableThread):
-    def __init__(self, port, *args, **kwargs):
-        super(ServiceAnnouncement, self).__init__(*args, **kwargs)
+    def __init__(self, port, MCAST_GRP = '224.1.1.1', MCAST_PORT = 5007):
+        super(ServiceAnnouncement, self).__init__()
         self.port = port
+        self.MCAST_GRP = MCAST_GRP
+        self.MCAST_PORT = MCAST_PORT
         logging.info("ServiceAnnouncement started")
 
-        self.udp_socket = socket(AF_INET, SOCK_DGRAM)  # create UDP socket
-        self.udp_socket.bind(("", 0))
-        self.udp_socket.setsockopt(
-            SOL_SOCKET, SO_BROADCAST, 1
-        )  # this is a broadcast socket
+        self.udp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        self.udp_socket.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 32)
         self.server_id = str(uuid.uuid4())
 
     def work_func(self):
         try:
             data = self.server_id
-            self.udp_socket.sendto(data.encode(), ("<broadcast>", self.port))
+            self.udp_socket.sendto(data.encode(), (self.MCAST_GRP, self.MCAST_PORT))
             logging.info("sent service announcement")
             sleep(5)
         except Exception as e:
--- src/localshit/service_discovery.py
+++ src/localshit/service_discovery.py
@@ -4,8 +4,9 @@ Service Announcement
 Adapted from https://stackoverflow.com/questions/21089268/python-service-discovery-advertise-a-service-across-a-local-network
 """
 
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import *
 from select import select
+import struct
 from stop import StoppableThread
 import logging
 
@@ -15,15 +16,29 @@ logging.basicConfig(
 
 
 class ServiceDiscovery(StoppableThread):
-    def __init__(self, port):
+    def __init__(self, port, MCAST_GRP = '224.1.1.1', MCAST_PORT = 5007):
         super(ServiceDiscovery, self).__init__()
         self.port = port
-        self.s = socket(AF_INET, SOCK_DGRAM)  # create UDP socket
-        self.s.bind(("", self.port))
+        self.MCAST_GRP = MCAST_GRP
+        self.MCAST_PORT = MCAST_PORT
+        self.udp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        try:
+            self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        except AttributeError:
+            pass
+        # self.udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32) 
+        # self.udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+
+        self.udp_socket.bind((self.MCAST_GRP, self.MCAST_PORT))
+
+        mreq = struct.pack("4sl", inet_aton(MCAST_GRP), INADDR_ANY)
+
+        self.udp_socket.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+
 
     def work_func(self):
 
-        inputready, outputready, exceptready = select([self.s], [], [], 1)
+        inputready, outputready, exceptready = select([self.udp_socket], [], [], 1)
         logging.info("waiting for connection...")
 
         for socket_data in inputready:
