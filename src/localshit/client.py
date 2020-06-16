7baf6dfc5ba23a80b99ee7bf91c047ac732bc297
Dave <david.luettmann@gmx.de>
2020-06-16 20:58:35 +0200

    add handle_messages

--- src/localshit/client.py
+++ src/localshit/client.py
@@ -23,6 +23,7 @@ class Client:
         new_message = "CL:%s" % (self.own_address)
         socket_unicast = utils.get_unicast_socket()
         socket_unicast.sendto(new_message.encode(), ("172.17.0.2", 10012))
+        
 
         socket_unicast.settimeout(1.0)
         data, server = socket_unicast.recvfrom(1024)
@@ -31,6 +32,32 @@ class Client:
             if parts[0] == "RP":
                 logging.info("Message received: %s" % parts[1])
                 self.leader_id = parts[1]
+                self.handle_messages(self.leader_id)
+
+    def handle_messages(self, server_ip):
+        self.socket_content.connect(("172.17.0.2", 6000))
+
+        # TODO: listen to proxy_server if new leader is there and to socket server
+        # bekomme ich es mit, wenn socket server nicht mehr verfügbar ist???
+        inputready, outputready, exceptready = select(
+            [self.socket_unicast, self.socket_content], [], [], 1
+        )
+
+        for socket_data in inputready:
+
+            data, addr = socket_data.recvfrom(1024)  # wait for a packet
+            if data:
+                parts = data.decode().split(":")
+                if parts[0] == "RP":
+                    # new leader there. break and handle_messages again with new_leader
+                elif parts[0] == "CO":
+                    logging.info(
+                        "Got content from %s: %s"
+                        % (addr[0], parts[1])
+                    )
+                else:
+                    pass
+
 
 
 if __name__ == "__main__":
