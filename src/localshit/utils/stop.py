ec13d209c2dfa7c1f09d8327f1787a6b630138de
Dave <david.luettmann@gmx.de>
2020-06-29 18:48:46 +0200

    add named threads

--- src/localshit/utils/stop.py
+++ src/localshit/utils/stop.py
@@ -6,7 +6,9 @@ class StoppableThread(threading.Thread):
     regularly for the stopped() condition."""
 
     def __init__(self, *args, **kwargs):
-        super(StoppableThread, self).__init__(*args, **kwargs)
+        super(StoppableThread, self).__init__(
+            name=self.__class__.__name__, *args, **kwargs
+        )
         self._stop_event = threading.Event()
         self._interval = 1
 
