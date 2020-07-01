ee9bf12c7b8f9fe7e6f21caac6df3b3114f40e99
Dave <david.luettmann@gmx.de>
2020-07-01 11:25:22 +0200

    add debug capability to localshit

--- src/localshit/__main__.py
+++ src/localshit/__main__.py
@@ -2,17 +2,30 @@ import logging
 import traceback
 import argparse
 from localshit.run import LocalsHitManager
+import debugpy
+import os
 
 
 def main():
     frontend = "192.168.0.179"
     parser = argparse.ArgumentParser(prog="my_megazord_program")
     parser.add_argument(
-        "-f", nargs="?", help='start with localshit -f "172.17.0.2" to add frontend'
+        "-f", "--frontend", help='start with localshit -f "172.17.0.2" to add frontend'
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        help="start localhost and wait for VS Code debugger",
+        action="store_true",
     )
     args = parser.parse_args()
-    if args.f:
-        frontend = args.f
+    if args.frontend:
+        frontend = args.frontend
+
+    if args.debug:
+        debugpy.listen(5678)
+        print("Waiting for debugger attach: %s" % os.getpid())
+        debugpy.wait_for_client()
 
     logging.info("Frontend server is set to %s" % frontend)
 
